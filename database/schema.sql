2db8683df65727c5e1010b59b1aa5ba4621d2c00
Matthew Mata <matthewmata1030@gmail.com>
2022-05-23 01:03:28 -0700

    Added Pictures table

--- database/schema.sql
+++ database/schema.sql
@@ -25,34 +25,30 @@ create table categories (
   unique index uq_category_id (category_id)
 );
 
+create table pictures (
+  picture_id bigint(20) not null auto_increment,
+  url varchar(100) not null,
+  primary key(picture_id),
+  post_id bigint(20),
+  unique index uq_picture_id (picture_id)
+  foreign key(posts_id) references posts(posts_id),
+);
+
 create table posts (
   posts_id bigint(20) not null auto_increment,
-  url_id varchar(36) default (uuid()),
+  url_id varchar(255) default (uuid()),
   title varchar(75) not null,
   price varchar(255),
-  title_city varchar(255),
+  city varchar(255),
   postal_code varchar(255) not null,
   description text not null,
   make varchar(255),
   model varchar(255),
   size varchar(255),
   condition_description varchar(255),
-  language varchar(255) not null,
-  cryptocurrency_ok boolean default 0,
-  delivery_ok boolean default 0,
-  email varchar(255),
-  show_phone_number boolean default 0,
-  phone_calls_ok boolean default 0,
-  text_sms_ok boolean default 0,
+  contact_name varchar(255) not null,
+  email varchar(255) not null,
   phone_number varchar(255),
-  extension varchar(255),
-  contact_name varchar(255),
-  show_address varchar(255),
-  street varchar(255),
-  cross_street varchar(255),
-  city varchar(255),
-  post_image_url_https varchar(255),
-  url varchar(255) not null,
   created_at timestamp default current_timestamp,
   category_id bigint(20) not null,
   user_id bigint(20) not null,
