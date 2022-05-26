56e49255917555cac9427dfba3d9b7171d63955b
Matthew Mata <matthewmata1030@gmail.com>
2022-05-26 01:18:02 -0700

    Added Home screen

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,10 +1,45 @@
-import Navbar from "../Components/Navbar"
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 
+import diamond from "../images/diamond.svg"
+import pirateShip from "../images/pirate-ship-black.png";
 
 const Home = () => {
   return (
     <div>
-        <Navbar/>
+      <Navbar />
+      <section class="hero is-fullheight is-info">
+        <div class="hero-body">
+          <div class="container">
+            <div className="columns">
+              <div className="hero-body column">
+                <div className="container has-text-centered">
+                  <p className="title">Hidden Treasures <img className="" src={diamond}></img></p>
+                  <p className="subtitle">
+                    Hidden Treasures is a website for viewing and posting local
+                    advertisements. It works a lot like the Classifieds section
+                    of a newspaper, and it's completely free to use. You can
+                    find a listing for almost anything on Hidden Treasures
+                    —bikes, electronics, videogames, used cars, jewelry, and a
+                    whole lot more.
+                  </p>
+                  <p className="subtitle">
+                    "One man's trash is another man's treasure" -Yotam
+                    Ottolenghi
+                  </p>
+                </div>
+              </div>
+              <div className="column">
+                <img src={pirateShip} />
+                <p className="subtitle has-text-centered">
+                  One man's trash is another man's treasure
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <Footer />
     </div>
   );
 };
